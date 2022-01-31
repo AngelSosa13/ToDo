2c921defcf67a646d6eed6ef7d6e197eacddb7e2
Angel Sebasthian Sosa <51758080+AngelSosa13@users.noreply.github.com>
2022-01-31 14:43:24 -0600

    Metodo post para crear nuevos task y guardado en la db

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const Task = require('../models/tasks');
+const BodyParser = require('body-parser');
 const router = express.Router();
+
+router.use(BodyParser.urlencoded({extended : true}));
 
 //RUTA INICIAL QUE DEVUELVE EL INDEX.HTML
 router.get("/", (req, res) => {
@@ -9,18 +12,24 @@ router.get("/", (req, res) => {
 });
 
 const show = async () => {
-    const show = await Task.find();
-    console.log(show);
+    const results = await Task.find();
+    //console.log(results);
 }
 
 //RUTA PARA POST
-router.get("/add-task", (req, res) => {
+router.post("/", (req, res) => {
+    var add = req.body.newInput;
     const NewTask = new Task({
-        taskBody: 'Lhiuebn'
+        taskBody: `${add}`
     });
     NewTask.save().then((result) => {res.send(result)}).catch((err) => {console.log(err)});
+    //console.log(NewTask);
+    res.redirect('/');
 });
 
+function Remove() {
+    
+}
 
 
 module.exports = router;
